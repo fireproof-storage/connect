607dcad23c59e0482256b431f29ea9d9648bd8f6
Marty Schoch <marty.schoch@gmail.com>
2024-08-12 09:57:34 -0400

    first attempt to reorg test

--- src/connector.test.ts
+++ src/connector.test.ts
@@ -19,69 +19,85 @@ describe("connector", () => {
     //   .setParam("endpoint", "http://127.0.0.1:9000")
     //   .URI();
     url = URI.from("file://./dist/connect_to?storekey=@bla@")
+
+    const sysfs = await rt.getFileSystem(url);
+    await sysfs.rm('./dist/connect_to', { recursive: true }).catch(() => {
+      /* */
+    });
+    await sysfs.rm('./dist/connector', { recursive: true }).catch(() => {
+      /* */
+    });
   });
   afterAll(() => {
     // unreg();
   });
-  it("should store and retrieve data", async () => {
-    console.log("--1")
-    const wdb = fireproof("my-database", {
-      store: {
-        stores: {
-          base: "file://./dist/connector?storekey=@bla@",
-        },
-      },
-    });
-    // db.connect("s3://testbucket/connector");
-    console.log("--2")
-    const connection = await connectionFactory(url);
-    console.log("--3")
-    await connection.connect_X(wdb.blockstore);
-
-    // await new Promise((res) => setTimeout(res, 1000));
-
-    console.log("--4")
-    const ran = Math.random().toString();
-    const count = 3;
-    for (let i = 0; i < count; i++) {
-      console.log("--4.01", i)
-      await wdb.put({ _id: `key${i}:${ran}`, hello: `world${i}` });
-      console.log("--4.02", i)
-    }
-    console.log("--4.1")
-    for (let i = 0; i < count; i++) {
-      expect(await wdb.get<{ hello: string }>(`key${i}:${ran}`)).toEqual({
-        _id: `key${i}:${ran}`,
-        hello: `world${i}`,
-      });
-    }
-    console.log("--5")
-    const docs = await wdb.allDocs();
-    console.log("--6")
-    expect(docs.rows.length).toBeGreaterThanOrEqual(count);
-    (await wdb.blockstore.loader?.WALStore())?.processQueue.waitIdle();
-    // console.log("--7")
-    await wdb.blockstore.destroy();
-    // console.log("--8")
-
-    const rdb = fireproof("", {
-      store: {
-        stores: {
-          base: url,
-          useEncryptedBlockstore: true
-        },
-      },
-    });
-    console.log("--9")
-    const rdocs = await rdb.allDocs();
-    // console.log("--10", rdocs)
-    expect(rdocs.rows.length).toBeGreaterThanOrEqual(count);
-    for (let i = 0; i < count; i++) {
-      expect(await rdb.get<{ hello: string }>(`key${i}:${ran}`)).toEqual({
-        _id: `key${i}:${ran}`,
-        hello: `world${i}`,
-      });
-    }
-    console.log("--11", rdocs.rows.length)
+  it("once", async () => {
+    await testInner(url)
+  });
+  it("twice", async () => {
+    await testInner(url)
+    await testInner(url)
   });
 });
+
+async function testInner(url) {
+  console.log("--1")
+  const wdb = fireproof("my-database", {
+    store: {
+      stores: {
+        base: "file://./dist/connector?storekey=@bla@",
+      },
+    },
+  });
+  // db.connect("s3://testbucket/connector");
+  console.log("--2")
+  const connection = await connectionFactory(url);
+  console.log("--3")
+  await connection.connect_X(wdb.blockstore);
+
+  // await new Promise((res) => setTimeout(res, 1000));
+
+  console.log("--4")
+  const ran = Math.random().toString();
+  const count = 3;
+  for (let i = 0; i < count; i++) {
+    console.log("--4.01", i)
+    await wdb.put({ _id: `key${i}:${ran}`, hello: `world${i}` });
+    console.log("--4.02", i)
+  }
+  console.log("--4.1")
+  for (let i = 0; i < count; i++) {
+    expect(await wdb.get<{ hello: string }>(`key${i}:${ran}`)).toEqual({
+      _id: `key${i}:${ran}`,
+      hello: `world${i}`,
+    });
+  }
+  console.log("--5")
+  const docs = await wdb.allDocs();
+  console.log("--6")
+  expect(docs.rows.length).toBeGreaterThanOrEqual(count);
+  (await wdb.blockstore.loader?.WALStore())?.processQueue.waitIdle();
+  // console.log("--7")
+  await wdb.blockstore.destroy();
+  // console.log("--8")
+
+  const rdb = fireproof("", {
+    store: {
+      stores: {
+        base: url,
+        useEncryptedBlockstore: true
+      },
+    },
+  });
+  console.log("--9")
+  const rdocs = await rdb.allDocs();
+  // console.log("--10", rdocs)
+  expect(rdocs.rows.length).toBeGreaterThanOrEqual(count);
+  for (let i = 0; i < count; i++) {
+    expect(await rdb.get<{ hello: string }>(`key${i}:${ran}`)).toEqual({
+      _id: `key${i}:${ran}`,
+      hello: `world${i}`,
+    });
+  }
+  console.log("--11", rdocs.rows.length)
+}
