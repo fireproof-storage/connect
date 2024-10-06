e69ad6192381d17e829803425c3a2211e113af36
Chris Anderson <jchris@gmail.com>
2024-10-05 18:32:08 -0700

    working on browser build

--- tsup.config.ts
+++ tsup.config.ts
@@ -19,15 +19,20 @@ const external = [
   "better-sqlite3",
 ];
 
-// const stopFile = {
-//   // "fs/promises": "../../../bundle-not-impl.js",
-//   // "../runtime/store-file.js": "../../bundle-not-impl.js",
-//   // "../runtime/gateways/file/gateway.js": "../bundle-not-impl.js",
-//   // "./mem-filesystem.js": "../../../bundle-not-impl.js",
-//   // "./gateways/file/gateway.js": "../bundle-not-impl.js",
-//   // "./node-sys-container.js": "../bundle-not-impl.js",
-//   // "./key-bag-file.js": "../bundle-not-impl.js",
-// };
+const stopFile = {
+  "fs/promises": "../../../bundle-not-impl.js",
+  "path": "../../../bundle-not-impl.js",
+  "assert": "../../../bundle-not-impl.js",
+  "util": "../../../bundle-not-impl.js",
+  "stream": "../../../bundle-not-impl.js",
+  "events": "../../../bundle-not-impl.js",
+  "../runtime/store-file.js": "../../bundle-not-impl.js",
+  "../runtime/gateways/file/gateway.js": "../bundle-not-impl.js",
+  "./mem-filesystem.js": "../../../bundle-not-impl.js",
+  "./gateways/file/gateway.js": "../bundle-not-impl.js",
+  "./node-sys-container.js": "../bundle-not-impl.js",
+  "./key-bag-file.js": "../bundle-not-impl.js",
+};
 
 const ourMultiformat = {
   // "multiformats/block": `${__dirname}/src/runtime/multiformat/block.ts`
@@ -132,6 +137,7 @@ const LIBRARY_BUNDLES: readonly Options[] = [
         include: /version/,
       }),
       resolve({
+        ...stopFile,
         ...ourMultiformat,
       }),
     ],
