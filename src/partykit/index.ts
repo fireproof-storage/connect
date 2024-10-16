5312ea3baa158f4ae52f77c21dbfc19358052d1c
Selem Delul <selem@fireproof.storage>
2024-10-16 17:05:03 -0400

    Typo

--- src/partykit/index.ts
+++ src/partykit/index.ts
@@ -66,7 +66,7 @@ async function getOrCreateRemoteName(dbName: string) {
 export function cloudConnect(
   db: Database,
   dashboardURI = BuildURI.from("https://dashboard.fireproof.storage/"),
-  partykitURL = '"http://localhost:1999?protocol=ws'
+  partykitURL = 'http://localhost:1999?protocol=ws'
 ) {
   const dbName = db.name;
   if (!dbName) {
