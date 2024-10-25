18012eb6ce9f7d4e7b7a5d18d2ad083ec1c5e0b4
Marty Schoch <marty.schoch@gmail.com>
2024-10-25 13:08:38 -0400

    fixes from call with Steven

--- src/ucan-cloud/store/state/index.ts
+++ src/ucan-cloud/store/state/index.ts
@@ -2,10 +2,10 @@ import { runtimeFn } from "@adviser/cement";
 
 export default async function store(name: string) {
   if (runtimeFn().isNodeIsh) {
-    const n = await import("./node.js");
+    const n = await import("../../../ucan-cloud/store/state/node.js");
     return n.default(name);
   } else {
-    const b = await import("./browser.js");
+    const b = await import("../../../ucan-cloud/store/state/node.js");
     return b.default(name);
   }
 }
