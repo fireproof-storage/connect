e37da5def58a6681c5384c85d1e9bdbf7e879946
Steven Vandevelde <steven@tokono.ma>
2024-10-30 19:41:10 +0100

    add demo

---
+++ examples/ucan-cloud/demo/src/index.ts
@@ -0,0 +1,102 @@
+import { Database, fireproof, type AllDocsResponse } from "@fireproof/core";
+import * as Connector from "@fireproof/connect/ucan";
+
+let clockId: string = "";
+let rows: AllDocsResponse<{}>["rows"] = [];
+
+// 🚀
+
+let db: Database<{}> | undefined;
+let databaseName = "my-db-0001";
+
+// Connection + Clock ID
+
+async function connect(ci?: `did:key:${string}`) {
+  if (db === undefined) return;
+
+  const email = undefined;
+
+  const connection = await Connector.connect(db, {
+    clock: ci ? Connector.clockId(ci) : undefined,
+    email,
+  });
+
+  clockId = (connection as any).url.getParam("clock-id");
+}
+
+// DB ROWS
+
+async function fetchRows() {
+  rows = db ? (await db.allDocs()).rows : [];
+}
+
+await fetchRows();
+
+// 🔮
+
+function render() {
+  const paragraph = document.body.querySelector("p");
+
+  if (paragraph)
+    paragraph.innerHTML = `
+    <strong>Clock ID:</strong> <span>${clockId}</span><br />
+
+    <code>${rows.length === 0 ? "No data" : JSON.stringify(rows)}</code>
+  `;
+}
+
+render();
+
+document.body.addEventListener("submit", (e) => e.preventDefault());
+
+// ADD DATA
+
+const add = document.querySelector("#add");
+
+if (add)
+  add.addEventListener("click", async (e) => {
+    // @ts-ignore
+    const val = document.querySelector("#add-input")?.value;
+    if (val) {
+      if (db === undefined) {
+        db = fireproof(databaseName);
+        await connect();
+      }
+      await db.put({ _id: Date.now().toString(), data: val });
+      await fetchRows();
+      render();
+    }
+  });
+
+// SWITCH
+
+const swtch = document.querySelector("#switch");
+
+if (swtch)
+  swtch.addEventListener("click", async (e) => {
+    // @ts-ignore
+    const val = document.querySelector("#switch-input")?.value;
+    if (val && val.startsWith("did:key:")) {
+      db = fireproof(databaseName);
+      await connect(val as `did:key:${string}`);
+      await fetchRows();
+      render();
+    }
+  });
+
+// RE-RENDER DATA
+
+const rerender = document.querySelector("#render-data");
+
+if (rerender)
+  rerender.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    if (db === undefined) {
+      db = fireproof(databaseName);
+      await connect();
+    }
+
+    await fetchRows();
+    render();
+  });
