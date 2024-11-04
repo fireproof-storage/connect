710954be084cc4f4c347e9d932bba1e9c884bb36
Steven Vandevelde <steven@tokono.ma>
2024-11-04 11:11:59 +0000

    reorg examples

--- examples/ucan-cloud/demo/rsbuild.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@rsbuild/core";
-import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
-
-export default defineConfig({
-  html: {
-    template: "./src/index.html",
-  },
-  plugins: [pluginNodePolyfill()],
-});
