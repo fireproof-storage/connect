d30ec876e73db1148e9c274caac408c3b3146624
J Chris Anderson <jchris@fireproof.storage>
2025-01-23 14:16:18 -0800

    try the ones that pass for me

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -2,24 +2,24 @@ import { defineWorkspace } from "vitest/config";
 
 import aws from "./vitest.aws.config.ts";
 import betterSqlite3 from "./vitest.better-sqlite3.config.ts";
-import nodeSqlite3Wasm from "./vitest.node-sqlite3-wasm.config.ts";
+// import nodeSqlite3Wasm from "./vitest.node-sqlite3-wasm.config.ts";
 import partykit from "./vitest.partykit.config.ts";
 import cloud from "./vitest.cloud.config.ts";
 import s3 from "./vitest.s3.config.ts";
 // import connector from "./vitest.connector.config.ts";
-import netlify from "./vitest.netlify.config.ts";
-import ucan from "./vitest.ucan.config.ts";
+// import netlify from "./vitest.netlify.config.ts";
+// import ucan from "./vitest.ucan.config.ts";
 import cfWorker from "./vitest.cf-worker.config.ts";
 
 export default defineWorkspace([
-  nodeSqlite3Wasm,
+  // nodeSqlite3Wasm,
   betterSqlite3,
   // connector,
   s3,
   aws,
-  netlify,
+  // netlify,
   partykit,
   cloud,
   cfWorker,
-  ucan,
+  // ucan,
 ]);
