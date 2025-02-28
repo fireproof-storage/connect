d2b0608cffae2babbe6203f72383384f0a59805a
Meno Abels <meno.abels@adviser.com>
2025-02-28 08:12:42 +0100

    chore: rename cloud-> v1-cloud to enable easy rebase

--- setup.v1-cloud.ts
+++ setup.v1-cloud.ts
@@ -1,10 +1,10 @@
-import { registerFireproofCloudStoreProtocol } from "./src/cloud/gateway.ts";
+import { registerFireproofCloudStoreProtocol } from "./src/v1-cloud/gateway.ts";
 import { BuildURI } from "@adviser/cement";
 
 registerFireproofCloudStoreProtocol();
 
 const url = BuildURI.from("fireproof://localhost:1998")
-  .setParam("getBaseUrl", "http://127.0.0.1:9000/testbucket/fp-cloud-test")
+  // .setParam("getBaseUrl", "http://127.0.0.1:9000/testbucket/fp-cloud-test")
   .setParam("protocol", "ws")
   .setParam("getNeedsAuth", "true");
 process.env.FP_STORAGE_URL = url.toString();
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -4,7 +4,7 @@ import aws from "./vitest.aws.config.ts";
 import betterSqlite3 from "./vitest.better-sqlite3.config.ts";
 // import nodeSqlite3Wasm from "./vitest.node-sqlite3-wasm.config.ts";
 import partykit from "./vitest.partykit.config.ts";
-import cloud from "./vitest.cloud.config.ts";
+import v1Cloud from "./vitest.v1-cloud.config.ts";
 import s3 from "./vitest.s3.config.ts";
 // import connector from "./vitest.connector.config.ts";
 import netlify from "./vitest.netlify.config.ts";
@@ -21,7 +21,7 @@ export default defineWorkspace([
   aws,
   netlify,
   partykit,
-  cloud,
+  v1Cloud,
   //cf_kv
   ucan,
 ]);
