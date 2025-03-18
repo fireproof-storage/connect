e6c30f013caf9735b3d1c8327fb5330ff7b6116d
Benraouane Soufiane <110255764+BenraouaneSoufiane@users.noreply.github.com>
2025-03-18 13:31:25 +0100

    Update drive-gateway.test.ts

--- src/drive/drive-gateway.test.ts
+++ src/drive/drive-gateway.test.ts
@@ -1,20 +1,12 @@
 import { fireproof } from "@fireproof/core";
-import { registerGDriveStoreProtocol } from "./drive-gateway.js";
+import { connect } from "./index.js";
 import { describe, it } from "vitest";
 import { smokeDB } from "../../tests/helper.js";
 
 describe("store-register", () => {
   it("should store and retrieve data", async () => {
-    const unreg = registerGDriveStoreProtocol("gdrive:");
-    const db = fireproof("my-database", {
-      store: {
-        stores: {
-          base: "gdrive://www.googleapis.com/?auth=testtoken",
-        },
-      },
-    });
+    const db = fireproof("my-database");
+    connect(db, "ya29.a0AeXRPp4bASF7IqO0gx7MYSjgV8zMixuMR3z4rWvuqYvNUKv9pDuPumJIvmaXzmNE7k-7xArIdN9-VRippEG5ByIoqinkRlyNxunGmLq9oGZvgAiQ6_zUxHBHm1XL2Nx2lBjILtZZkJlFRNc_02wE5GZJ2VkLQar_rS-BKOqPaCgYKAfcSAQ8SFQHGX2Mia-YkdQYWm7KsWLbfF2ZwOQ0175")
     await smokeDB(db);
-    await db.destroy();
-    unreg();
   });
 });
