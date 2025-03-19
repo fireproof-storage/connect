226992d93cbbc1305564718f46c327025cd2333f
Benraouane Soufiane <110255764+BenraouaneSoufiane@users.noreply.github.com>
2025-03-19 09:21:46 +0100

    Update drive-gateway.test.ts

--- src/drive/drive-gateway.test.ts
+++ src/drive/drive-gateway.test.ts
@@ -1,12 +1,16 @@
 import { fireproof } from "@fireproof/core";
-import { connect } from "./index.js";
+import {registerGDriveStoreProtocol} from "./drive-gateway.ts"
 import { describe, it } from "vitest";
 import { smokeDB } from "../../tests/helper.js";
 
-describe("store-register", () => {
-  it("should store and retrieve data", async () => {
-    const db = fireproof("my-database");
-    connect(db, "ya29.a0AeXRPp4bASF7IqO0gx7MYSjgV8zMixuMR3z4rWvuqYvNUKv9pDuPumJIvmaXzmNE7k-7xArIdN9-VRippEG5ByIoqinkRlyNxunGmLq9oGZvgAiQ6_zUxHBHm1XL2Nx2lBjILtZZkJlFRNc_02wE5GZJ2VkLQar_rS-BKOqPaCgYKAfcSAQ8SFQHGX2Mia-YkdQYWm7KsWLbfF2ZwOQ0175")
+describe("store-register", { timeout: 100000 }, () => {
+  it("should store and retrieve data", { timeout: 100000 },  async () => {
+    registerGDriveStoreProtocol("gdrive:", "ya29.a0AeXRPp7h4fANs0x2Y9nL3TCvL96bAnUJmwQ0oOlXPcKSmnajd_h8X2yxA8vdUo62CiKySJzrhYHMhwQVqvLnNVrHaSgR23PuF6rZXLXMApAu6rfRWtVFFKS8pYjEm36VW5csE656Z5bHXzWLXitqz9we-7zskpMNbak_NWOMaCgYKAXoSAQ8SFQHGX2MiXQzoB1I3l33KyL1DJICHNw0175")
+    const db = fireproof("diy-0.0", {
+      storeUrls: {
+        base: "gdrive://home-improvement",
+      },
+    });
     await smokeDB(db);
   });
 });
